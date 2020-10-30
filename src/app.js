462cec0616feacb879453d8ffb22f66043ea6602
cfloyd252 <cfloyd252@gmail.com>
2020-10-30 06:19:53 -0700

    auth router completed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+app.use('/api/auth', authRouter)
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
+'use strict';
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-}
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/tandem-code-challenge',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/tandem-code-challenge-test',
+  JWT_SECRET: process.env.JWT_SECRET || 'secret-secret',
+};
